1a1cef4625e70b56982c8b7e9a89a489c3b3182b
Greg Putzel <gregputzel@gmail.com>
2018-03-16 16:39:20 -0400

    Use zcat instead of gzcat for Linux compatibility

--- data/Russo2018FrontMicro/scripts/fastqc.sh
+++ data/Russo2018FrontMicro/scripts/fastqc.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-FASTQC=/Applications/FastQC.app/Contents/MacOS/fastqc
 cd FASTQ
 for f in $(ls *.fastq.gz)
 do
     f2=${f%.fastq.gz}
     name=${f2/pass_/}
     echo $name
-    gzcat $f | $FASTQC stdin; mv stdin_fastqc.html ../FASTQC/"$name"_FastQC.html
+    zcat $f | fastqc stdin; mv stdin_fastqc.html ../FASTQC/"$name"_FastQC.html
     rm stdin_fastqc.zip
 done
