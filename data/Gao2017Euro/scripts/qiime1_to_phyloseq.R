6b48aa2821861d89d657b44674ccdfb5038028c6
Gregory Putzel <grp2009@med.cornell.edu>
2018-04-06 10:52:00 -0400

    Script to generate phyloseq object from qiime1 output

--- data/Gao2017Euro/scripts/qiime1_to_phyloseq.R
+++ data/Gao2017Euro/scripts/qiime1_to_phyloseq.R
@@ -7,17 +7,27 @@ ps <- import_biom(biom.file)
 mytree <- read_tree(snakemake@input[['tree']])
 phy_tree(ps) <- mytree
 
+ps
+
 sample.df <- read.table('sample_data/SraRunTable.txt',sep='\t',header=TRUE)
 rownames(sample.df) <- sample.df[,'Run']
 
-sample.df <- sample.df[,c('Library_Name','Sample_Name',
-                          'chem_administration','ethnicity',
-                          'host_tissue_sampled','env_material')]
+sample.df <- sample.df[,c('Library_Name','Sample_Name')]
 
 sample.df.reordered <- sample.df[sample_names(ps),]
 
-sample.df.reordered$Group <- as.character(sample.df.reordered$ethnicity)
-sample.df.reordered$Subject <- as.character(sample.df.reordered$chem_administration)
+sample.df.reordered$Sample_Name <- as.character(sample.df.reordered$Sample_Name)
+
+#Some of the sample names have underscores, others not
+sample.names <- gsub('_','',sample.df.reordered$Sample_Name)
+types <- ifelse(grepl('T$',sample.names),'T','Other')
+types[grepl('N$',sample.names)] <- 'N'
+types[grepl('C$',sample.names)] <- 'C'
+sample.df.reordered$type <- types
+sample.df.reordered$subject <- gsub('T','',gsub('C','',gsub('N','',sample.names)))
+
+#I'm assuming that "C" and "T" mean the same thing. Will confirm with the authors
+sample.df.reordered$type <- gsub('C','T',sample.df.reordered$type)
 
 sample_data(ps) <- sample_data(sample.df.reordered)
 
